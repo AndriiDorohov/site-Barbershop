2a0a2d6af663365da756f054c393840546962dee
Andrii Dorokhov <apogee@MacBook-Pro-Andrii.local>
2024-01-08 23:57:44 +0100

    add files to repository

---
+++ js/id-scroll.js
@@ -0,0 +1,9 @@
+
+ $(document).ready(function(){
+    $("#scroll").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
